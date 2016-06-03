2e2fe06c832c3ec6497c91332f2895610ff58515
gatorgator <18221869670@163.com>
2016-06-03 18:27:25 +0800

    add miniproject & ch2 running code

---
+++ miniproject/MovieRecommend.py
@@ -0,0 +1,87 @@
+#!/home/xfanwang/anaconda/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+def load_matrix():
+    matrix = {}
+    f = open("./train.csv")
+    columns = f.readline().split(',')
+    print "columns:", columns
+
+    for line in f:
+        scores = line.split(',')
+        print "line:", scores
+    for i in range(len(scores))[1:]:
+        matrix[(scores[0], columns[i])] = scores[i].strip("\n")
+    return matrix
+
+def sim_distance(matrix, row1, row2):
+    columns = set(map(lambda l: l[1], matrix.keys()))
+    si = filter(lambda l: matrix.has_key((row1, l)) and matrix[(row1, l)] != "" and matrix.has_key((row2, l)) and matrix[(row2, l)] != "", columns)
+    if len(si) == 0: return 0
+    sum_of_distance = sum([pow(float(matrix[(row1, column)]) - float(matrix[(row2, column)]), 2) for column in si])
+    return 1 / (1 + math.sqrt(sum_of_distance))
+                 
+def top_matches(matrix, row, similarity=sim_distance):
+    rows = set(map(lambda l: l[0], matrix.keys()))
+    scores = [(similarity(matrix, row, r), r) for r in rows if r != row]
+    scores.sort()
+    scores.reverse()
+    return scores
+
+def transform(matrix):
+    rows = set(map(lambda l: l[0], matrix.keys()))
+    columns = set(map(lambda l: l[1], matrix.keys()))
+         
+    transform_matrix = {}
+    for row in rows:
+        for column in columns:
+            transform_matrix[(column, row)] = matrix[(row, column)]
+    return transform_matrix
+                 
+def get_recommendations(matrix, row, similarity=sim_distance):
+    rows = set(map(lambda l: l[0], matrix.keys()))
+        columns = set(map(lambda l: l[1], matrix.keys()))
+         
+             sum_of_column_sim = {}
+                 sum_of_column = {}
+                  
+                      for r in rows:
+                                  if r == row: continue
+                                          sim = similarity(matrix, row, r)
+                                                  if sim <= 0:  continue
+                                                   
+                                                           for c in columns:
+                                                               if matrix[(r, c)] == "": continue
+                                                                
+                                                                            sum_of_column_sim.setdefault(c, 0)
+                                                                                        sum_of_column_sim[c] += sim
+                                                                                                    sum_of_column.setdefault(c, 0)
+                                                                                                                sum_of_column[c] += float(matrix[(r, c)]) * sim
+                                                                                                                 
+                                                                                                                     scores = [(sum_of_column[c] / sum_of_column_sim[c], c) for c in sum_of_column]
+                                                                                                                         scores.sort()
+                                                                                                                             scores.reverse()
+                                                                                                                                 return scores
+
+if __name__=="__main__":
+    matrix = load_matrix()
+    print "matrix:", matrix
+    print "--->"
+    print sim_distance(matrix, "Kai Zhou", "Shuai Ge")
+    print "--->"
+    person = "Kai Zhou"
+    print "top match for:", person
+    print top_matches(matrix, person)
+    print "--->"
+    trans_matrix = transform(matrix)
+    print "trans:", trans_matrix
+     
+    film = "Friends"
+    print "top match for:", film
+    print top_matches(trans_matrix, film)
+
+    print "--->"
+    trans_matrix = transform(matrix)
+    print get_recommendations(trans_matrix,  "Friends")
